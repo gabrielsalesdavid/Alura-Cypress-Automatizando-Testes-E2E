af341340c8a90312bffed95334da4d4abdf232c0
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-08-31 17:21:19 -0300

    Cypress Cirando Teste

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Maria Calra');
+    cy.get('input[name="email"]').type('maria@gmail.com');
+    cy.get('input[name="password"]').type('Senha123');
+    cy.get('input[name="confirm_password"]').type('Senha123');
+    cy.contains('button', 'Cadastrar').click();
+  })
+})
