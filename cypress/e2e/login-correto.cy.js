c3b6ca22580927fe2e1e9422ee8bcae1593ec9aa
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-08-31 22:42:41 -0300

    Chomawesome

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -4,8 +4,6 @@ describe('Página de Cadastro', () => {
     cy.get('[data-test="login-button"]').click();
     })
   it('Deve preencher os campos de login corretamente e autenticar o usuário na página', () => {
-    cy.get('[data-test="input-loginEmail"]').type('maria@gmail.com');
-    cy.get('[data-test="input-loginPassword"]').type('Senha123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('maria@gmail.com', 'Senha123');
   })
 })
