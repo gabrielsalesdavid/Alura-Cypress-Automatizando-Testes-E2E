2118c89c57ed6be22ef8bdc7eeac39eeae223453
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-09-06 09:29:07 -0300

    Aplicando boas praticas com Cypress

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,16 +1,15 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-    eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZX
-    3QchEOpTJPumDhlVJULXURUa44Py3gSax4Pj5lep2q4`
+    
     it('Mensagens da API', () => {
         CSSSkewY.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { autorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(Response.status).to.be.equal(200)
             expect(res.body).is.not.empty
             expect(res.body).to.have.property('msg')
+            expect(res.duration).to.be.lte(tempoEsperado)
         })
     })
 })
